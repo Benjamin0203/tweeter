70aec029deff7bc5c2b3934a5f12fc281dca8dee
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-11-08 23:11:44 -0800

    Created two functions: createTweetElement and renderTweets to append the html #tweets-container secion

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,8 +6,35 @@
 
 // const $tweet = $(`<article class="tweet">Hello world</article>`);
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
 const createTweetElement = tweetData => {
-  const $tweet = $(`
+  let $tweet = $(`
   <article class="tweet">
           <header class="tweet-header">
             <div class="tweet-profile">
@@ -16,7 +43,7 @@ const createTweetElement = tweetData => {
             </div>
           </header>
           <div class="tweet-comment">
-            $escape(tweetData.content.text)
+            ${tweetData.content.text}
           </div>
           <footer class="tweet-new-footer">
             <span class="tweet-date">${tweetData.created_at}</span>
@@ -31,22 +58,19 @@ const createTweetElement = tweetData => {
   return $tweet;
 };
 
+const renderTweets = tweets => {
+  $('#tweet-container').empty();
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweet-container').prepend($tweet);
+  }
+};
 
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
 
-const $tweet = createTweetElement(tweetData);
+$(() => {
+  renderTweets(data);
+});
 
 
 // Test / driver code (temporary)
